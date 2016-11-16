1555cc3e75735c6b2d4e29e5abb480355f8a6b42
Evgeniy Kobrikov <kobrikov@yandex.ru>
2016-11-16 19:26:41 +0300

    Подключил интерактивные элементы

---
+++ js/feedback.js
@@ -0,0 +1,59 @@
+var link = document.querySelector(".btn-feedback");
+var popup = document.querySelector(".popup-feedback");
+var overlay = document.querySelector(".popup-overlay");
+
+var name = document.querySelector("[name=popup-name]");
+var email = document.querySelector("[name=popup-email]");
+var message = document.querySelector("[name=popup-message]");
+
+var close = popup.querySelector(".popup-close");
+var form = popup.querySelector("form");
+
+link.addEventListener("click", function (event) {
+	
+	event.preventDefault();
+	popup.classList.add("popup-feedback-show");
+	overlay.classList.add("popup-overlay-show");
+
+	if (localStorage.getItem("name") && localStorage.getItem("email")) {
+		name.value = localStorage.getItem("name");
+		email.value = localStorage.getItem("email");
+		message.focus();
+	}
+
+});
+
+form.addEventListener("submit", function (event) {
+
+	if (!name.value && !email.value) {
+		event.preventDefault();
+	} else {
+		localStorage.setItem("name", name.value);
+		localStorage.setItem("email", email.value);
+	}
+
+});
+
+close.addEventListener("click", function (event) {
+	
+	popup.classList.remove("popup-feedback-show");
+	overlay.classList.remove("popup-overlay-show");
+
+});
+
+overlay.addEventListener("click", function (event) {
+	
+	popup.classList.remove("popup-feedback-show");
+	overlay.classList.remove("popup-overlay-show");
+	
+});
+
+window.addEventListener("keydown", function (event) {
+	
+	if (event.keyCode === 27) {
+		if (popup.classList.contains("popup-feedback-show")) {
+			popup.classList.remove("popup-feedback-show");
+			overlay.classList.remove("popup-overlay-show");
+		}
+	}
+});
